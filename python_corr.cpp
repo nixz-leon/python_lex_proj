b060d7b89d93021250e455d7ab5eedeaf6a118a6
Leon <nixzleon@gmail.com>
2024-02-26 15:56:56 -0700

    Reworked the logic to work on a rolling 3 line window rather than line by line with an indent count, advantages of this, is easier handeling of certain edge cases of the line by line approach

--- python_corr.cpp
+++ python_corr.cpp
@@ -26,14 +26,16 @@ inline string condition_line(string input){
     int temp = (int)(((float)space_count/4.f)+0.5f); 
     indent_count += temp;
     //now just need to need to add a char to represent level of indentation
-    input = tochar(indent_count) + input.substr(space_count,input.length());
+    input.erase(0,space_count);
+    input = tochar(indent_count) + input; 
+    while(isspace(input.back())){//removing white spaces from each string
+        input.erase(input.size()-1);
+    }
     return input;
 }
 
 inline bool check_print(string input){
-    if(input.substr(1,6) == "print(" && (input[input.size()-2] == ')' || input.back() == ')')){
-        return true;
-    }
+    if(input.substr(1,6) == "print(" && input.back() == ')'){return true;}
     return false;
 }
 
@@ -45,20 +47,6 @@ inline string uncondition_line(string input){
     input = temp + input.substr(1,input.size());
     return input;
 }
-
-inline string check_keyword(string input){
-    input = input.substr(1,input.size());
-    string keyword = "";
-    
-    // first need to ident key word
-    // if no key word then exit
-    // if first char is the comment symbol, exit it doesn't matter
-    // then check if key word is using proper syntax
-    // if key word has proper syntax return key word identified
-
-    return keyword;
-}   
-
 
 int main(){
     vector<string> lines;
@@ -72,74 +60,48 @@ int main(){
 
 
     string line ="";
+    int g =0;
     while(getline(file,line)){   
         if(line != ""){
             outfile << line << endl;
-            lines.push_back(condition_line(line));   
+            lines.push_back(condition_line(line));
+            print_count += (int)check_print(lines[g]);   
+            g++;
         }
     }
-
-
-    int indentation=0;
     int loop_count = 0;
-    int curr_indent =0;
-    int min_statement = 0;
+    int n1;int n2 = toint(lines[0][0]);int n3 = toint(lines[1][0]);
+    char e1; char e2= lines[0][lines[0].size()-1];char e3= lines[1][lines[1].size()-1];
 
-    for(int i=0; i < (int)lines.size(); i++){
-        cout << lines[i] << endl;
-    }
 
-    for(int i=0; i < (int)lines.size(); i++){
-        print_count += (int)check_print(lines[i]);  
-        if(lines[i][lines[i].size()-2] == ':'){
-            //need to add indentation check here
-            if(toint(lines[i][0]) != indentation){
-                lines[i][0] = tochar(indentation);
-            }
-            indentation++;
+    for(int i =0; i < (int)lines.size()-2; i++){
+        n1 = n2; n2 = n3; // indent count, n1, n2, n3, since we iterate only line line at a time, 
+        n3 = toint(lines[i+2][0]);
+        e1 = e2; e2 = e3; // these are ending char, makes the rest of the code more readable
+        e3 = lines[i+2].back();
+        if(e1 == ':'){
             loop_count++;
-            min_statement = 1;
-            //here just need to check for char d at position 1; if so call the correct function function
-            
-        }else{
-            //For logic diagram, refer to pdf posted in the discord
-            curr_indent = toint(lines[i][0]);
-            if(curr_indent!=indentation){
-                if(min_statement == 0){//here i have min_statement = 0, which in this case is the inverse of min_statment = 1;
-                    if(curr_indent <= indentation-loop_count){
-                        if(lines[i-1][0] == lines[i+1][0]){
-                            cout << lines[i+1] << endl;
-                            cout << indentation << endl;
-                            curr_indent = indentation;
-                        }else{
-                            indentation -= loop_count;
-                            curr_indent = indentation;
-                            loop_count = 0;
-                        }
-                        
-                    }else if(curr_indent < indentation){
-                        if(lines[i-1][0] == lines[i+1][0]){
-                            cout << lines[i+1] << endl;
-                            cout << indentation << endl;
-                            curr_indent = indentation;
-                        }else{
-                            loop_count -= indentation-curr_indent;
-                            indentation = curr_indent;
-                        }
-                    }else{
-                        curr_indent = indentation;
-                    }
-                }else{
-                    curr_indent = indentation;
-                    min_statement = 0;
-                }
-            }else{
-                min_statement = 0;
-            }
-            cout << indentation << endl;
-            lines[i][0]= tochar(curr_indent);
+            if((n1+1)!=n2){n2 = n1+1;}
+            if(n2<n3){n3=n2;};
         }
+        else if((e2==e1) & (e1 == ':')){n2 = n1+1;n3 = n2+1;}
+        else if ((e2==e3) & (e2 == ':')){n2 = n1; n3 = n2+1;}
+        else if (e2 == ':'){if(n1!=n3){n3=n1;}}
+        else{
+            cout << "con 4" << endl;
+            if(n1==n3){n2=n3;}
+            else if((n1==n2) & (n2>n3)){loop_count = n2-n3;}
+            else if((n1==n2) & (n2<n3)){n3=n2;}
+            else if((n2==n3) & (n1>n2)){loop_count=n2;}
+            else if((n2==n3) & (n1<n2)){n2=n1;n3=n1;}
+        }
+        lines[i][0] = tochar(n1);
+        lines[i+1][0] = tochar(n2);
+        lines[i+2][0] = tochar(n3);
+        cout << endl << endl;
     }
+
+
 
     outfile << " " << endl;//adding seperation between the two files
     outfile << " " << endl;
@@ -149,6 +111,7 @@ int main(){
     }
     outfile << endl << endl << endl;
     outfile << print_count << endl;
+    
     
 }
 
--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 def funs(a, b):
     ahs = 2
-    shd = 3 
+    shd = 3
     for i in range(0,1):
         for j in range(0,1):
-                print("hello")
+            print("hello")
         print("hello")
-                print("hello")
-        
+            print("hello")
+        for k in range(0,1):
+            print("hello")
 print("hello")
 print("asda")
